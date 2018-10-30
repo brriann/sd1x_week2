5c773670d36c113c48b9f91c2735aa1545df3b0e
Brian Foster <brriann@gmail.com>
2018-10-29 22:03:17 -0600

    clean up unit tests

--- SquarelotronTest.java
+++ SquarelotronTest.java
@@ -44,25 +44,25 @@ class SquarelotronTest {
 		//flip inner ring
 		Squarelotron sq4_ud2 = sq4.upsideDownFlip(2);
 		
-		assertEquals(sq4.squarelotron[0][0], sq4_ud1.squarelotron[0][0]);
-		assertEquals(sq4.squarelotron[0][1], sq4_ud1.squarelotron[0][1]);
-		assertEquals(sq4.squarelotron[0][2], sq4_ud1.squarelotron[0][2]);
-		assertEquals(sq4.squarelotron[0][3], sq4_ud1.squarelotron[0][3]);
+		assertEquals(sq4.squarelotron[0][0], sq4_ud2.squarelotron[0][0]);
+		assertEquals(sq4.squarelotron[0][1], sq4_ud2.squarelotron[0][1]);
+		assertEquals(sq4.squarelotron[0][2], sq4_ud2.squarelotron[0][2]);
+		assertEquals(sq4.squarelotron[0][3], sq4_ud2.squarelotron[0][3]);
+	
+		assertEquals(sq4.squarelotron[3][0], sq4_ud2.squarelotron[3][0]);
+		assertEquals(sq4.squarelotron[3][1], sq4_ud2.squarelotron[3][1]);
+		assertEquals(sq4.squarelotron[3][2], sq4_ud2.squarelotron[3][2]);
+		assertEquals(sq4.squarelotron[3][3], sq4_ud2.squarelotron[3][3]);
+	
+		assertEquals(sq4.squarelotron[1][0], sq4_ud2.squarelotron[1][0]);
+		assertEquals(sq4.squarelotron[2][0], sq4_ud2.squarelotron[2][0]);
+		assertEquals(sq4.squarelotron[1][3], sq4_ud2.squarelotron[1][3]);
+		assertEquals(sq4.squarelotron[2][3], sq4_ud2.squarelotron[2][3]);
 		
-		assertEquals(sq4.squarelotron[3][0], sq4_ud1.squarelotron[3][0]);
-		assertEquals(sq4.squarelotron[3][1], sq4_ud1.squarelotron[3][1]);
-		assertEquals(sq4.squarelotron[3][2], sq4_ud1.squarelotron[3][2]);
-		assertEquals(sq4.squarelotron[3][3], sq4_ud1.squarelotron[3][3]);
-		
-		assertEquals(sq4.squarelotron[1][0], sq4_ud1.squarelotron[1][0]);
-		assertEquals(sq4.squarelotron[2][0], sq4_ud1.squarelotron[2][0]);
-		assertEquals(sq4.squarelotron[1][3], sq4_ud1.squarelotron[1][3]);
-		assertEquals(sq4.squarelotron[2][3], sq4_ud1.squarelotron[2][3]);
-		
-		assertEquals(sq4.squarelotron[1][1], sq4_ud1.squarelotron[2][1]);
-		assertEquals(sq4.squarelotron[1][2], sq4_ud1.squarelotron[2][2]);
-		assertEquals(sq4.squarelotron[2][1], sq4_ud1.squarelotron[1][1]);
-		assertEquals(sq4.squarelotron[2][2], sq4_ud1.squarelotron[1][2]);
+		assertEquals(sq4.squarelotron[1][1], sq4_ud2.squarelotron[2][1]);
+		assertEquals(sq4.squarelotron[1][2], sq4_ud2.squarelotron[2][2]);
+		assertEquals(sq4.squarelotron[2][1], sq4_ud2.squarelotron[1][1]);
+		assertEquals(sq4.squarelotron[2][2], sq4_ud2.squarelotron[1][2]);
 		
 		
 		// test that a ring past ring limit returns equal sq.
@@ -102,27 +102,27 @@ class SquarelotronTest {
 		Squarelotron sq5_ud2 = sq5.upsideDownFlip(2);
 		
 		for (int j = 0; j < 5; j++) {
-			assertEquals(sq5.squarelotron[0][j], sq5_ud1.squarelotron[0][j]);
+			assertEquals(sq5.squarelotron[0][j], sq5_ud2.squarelotron[0][j]);
 		}
 		for (int j = 0; j < 5; j++) {
-			assertEquals(sq5.squarelotron[4][j], sq5_ud1.squarelotron[4][j]);
+			assertEquals(sq5.squarelotron[4][j], sq5_ud2.squarelotron[4][j]);
 		}
 		for (int j = 0; j < 5; j++) {
-			assertEquals(sq5.squarelotron[2][j], sq5_ud1.squarelotron[2][j]);
+			assertEquals(sq5.squarelotron[2][j], sq5_ud2.squarelotron[2][j]);
 		}
-		assertEquals(sq5.squarelotron[1][0], sq5_ud1.squarelotron[1][0]);
-		assertEquals(sq5.squarelotron[3][0], sq5_ud1.squarelotron[3][0]);
-		assertEquals(sq5.squarelotron[1][4], sq5_ud1.squarelotron[1][4]);
-		assertEquals(sq5.squarelotron[3][4], sq5_ud1.squarelotron[3][4]);
+		assertEquals(sq5.squarelotron[1][0], sq5_ud2.squarelotron[1][0]);
+		assertEquals(sq5.squarelotron[3][0], sq5_ud2.squarelotron[3][0]);
+		assertEquals(sq5.squarelotron[1][4], sq5_ud2.squarelotron[1][4]);
+		assertEquals(sq5.squarelotron[3][4], sq5_ud2.squarelotron[3][4]);
 		
 		
 		
-		assertEquals(sq5.squarelotron[1][1], sq5_ud1.squarelotron[3][1]);
-		assertEquals(sq5.squarelotron[1][2], sq5_ud1.squarelotron[3][2]);
-		assertEquals(sq5.squarelotron[1][3], sq5_ud1.squarelotron[3][3]);
-		assertEquals(sq5.squarelotron[3][1], sq5_ud1.squarelotron[1][1]);
-		assertEquals(sq5.squarelotron[3][2], sq5_ud1.squarelotron[1][2]);
-		assertEquals(sq5.squarelotron[3][3], sq5_ud1.squarelotron[1][3]);
+		assertEquals(sq5.squarelotron[1][1], sq5_ud2.squarelotron[3][1]);
+		assertEquals(sq5.squarelotron[1][2], sq5_ud2.squarelotron[3][2]);
+		assertEquals(sq5.squarelotron[1][3], sq5_ud2.squarelotron[3][3]);
+		assertEquals(sq5.squarelotron[3][1], sq5_ud2.squarelotron[1][1]);
+		assertEquals(sq5.squarelotron[3][2], sq5_ud2.squarelotron[1][2]);
+		assertEquals(sq5.squarelotron[3][3], sq5_ud2.squarelotron[1][3]);
 		
 		
 		// test that a ring past ring limit returns equal sq.
